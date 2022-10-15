8f4c4476328bfb2f49d7c8a057df75158c89a867
Soham Goswami <soham@chaka.info>
2022-10-15 17:47:37 +0530

    feat: refactored time frame

--- internal/timeframe/timeframe.go
+++ internal/timeframe/timeframe.go
@@ -1,29 +1,32 @@
 package timeframe
 
-import "time"
+type TimeFrame uint64
 
-type TimeFrame time.Duration
+const (
+	Minute uint64 = 1
+	Hour          = 60 * Minute
+)
 
 func PerMinute() TimeFrame {
-	return newDuration(1, time.Minute)
+	return newDuration(1, Minute)
 }
 
 func PerNMinute(n uint8) TimeFrame {
-	return newDuration(n, time.Minute)
+	return newDuration(uint64(n), Minute)
 }
 
 func PerHour() TimeFrame {
-	return newDuration(1, time.Hour)
+	return newDuration(1, Hour)
 }
 
 func PerNHours(n uint8) TimeFrame {
-	return newDuration(n, time.Hour)
+	return newDuration(uint64(n), Hour)
 }
 
-func newDuration(multiple uint8, period time.Duration) TimeFrame {
-	return TimeFrame(time.Duration(multiple) * period)
+func newDuration(multiple uint64, period uint64) TimeFrame {
+	return TimeFrame(multiple * period)
 }
 
-func (tf TimeFrame) Duration() time.Duration {
-	return time.Duration(tf)
+func (tf TimeFrame) Duration() uint64 {
+	return uint64(tf)
 }
--- internal/timeframe/timeframe_test.go
+++ internal/timeframe/timeframe_test.go
@@ -4,24 +4,23 @@ import (
 	"github.com/0xf0f17a/tau/internal/timeframe"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"time"
 )
 
 var _ = Describe("Timeframe", func() {
 	It("Has Per minute timeframe", func() {
 		tf := timeframe.PerMinute()
-		Expect(tf.Duration()).To(Equal(1 * time.Minute))
+		Expect(tf.Duration()).To(Equal(1 * timeframe.Minute))
 	})
 	It("Has Per N minute timeframe", func() {
 		tf := timeframe.PerNMinute(15)
-		Expect(tf.Duration()).To(Equal(15 * time.Minute))
+		Expect(tf.Duration()).To(Equal(15 * timeframe.Minute))
 	})
 	It("Has Per Hour timeframe", func() {
 		tf := timeframe.PerHour()
-		Expect(tf.Duration()).To(Equal(1 * time.Hour))
+		Expect(tf.Duration()).To(Equal(1 * timeframe.Hour))
 	})
 	It("Has Per N Hour timeframe", func() {
 		tf := timeframe.PerNHours(15)
-		Expect(tf.Duration()).To(Equal(15 * time.Hour))
+		Expect(tf.Duration()).To(Equal(15 * timeframe.Hour))
 	})
 })
