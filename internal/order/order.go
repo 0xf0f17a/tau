e920a1e4c9237f17caf0ee6deed0699e10261f45
Soham Goswami <soham@chaka.info>
2022-10-15 20:29:15 +0530

    feat: create unit tests for order

---
+++ internal/order/order.go
@@ -0,0 +1,27 @@
+package order
+
+import (
+	"github.com/0xf0f17a/tau/internal/instr"
+	"github.com/0xf0f17a/tau/internal/value"
+	"time"
+)
+
+type Order struct {
+	Instr instr.Instrument
+	Price value.Value
+	Time  time.Time
+	Count uint64
+	Type  Type
+}
+
+func BuyOrder(instr instr.Instrument, price value.Value, time time.Time, count uint64) *Order {
+	return newOrder(instr, price, time, count, Buy)
+}
+
+func SellOrder(instr instr.Instrument, price value.Value, time time.Time, count uint64) *Order {
+	return newOrder(instr, price, time, count, Sell)
+}
+
+func newOrder(instr instr.Instrument, price value.Value, time time.Time, count uint64, t Type) *Order {
+	return &Order{instr, price, time, count, t}
+}
---
+++ internal/order/order_test.go
@@ -0,0 +1,33 @@
+package order_test
+
+import (
+	"github.com/0xf0f17a/tau/internal/instr"
+	"github.com/0xf0f17a/tau/internal/order"
+	"github.com/0xf0f17a/tau/internal/value"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"time"
+)
+
+var _ = Describe("Order", func() {
+	inst := instr.New("InstrumentName")
+	price := value.NewFloat(343)
+	timeStamp := time.Now()
+	var count uint64 = 1
+	It("Should create valid buy order", func() {
+		ord := order.BuyOrder(inst, price, timeStamp, count)
+		Expect(ord.Instr).To(Equal(inst))
+		Expect(ord.Price).To(Equal(price))
+		Expect(ord.Time).To(Equal(timeStamp))
+		Expect(ord.Count).To(Equal(count))
+		Expect(ord.Type).To(Equal(order.Buy))
+	})
+	It("Should create valid sell order", func() {
+		ord := order.SellOrder(inst, price, timeStamp, count)
+		Expect(ord.Instr).To(Equal(inst))
+		Expect(ord.Price).To(Equal(price))
+		Expect(ord.Time).To(Equal(timeStamp))
+		Expect(ord.Count).To(Equal(count))
+		Expect(ord.Type).To(Equal(order.Sell))
+	})
+})
