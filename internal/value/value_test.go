fc93ca70b86f9581fb6fe295af0290d519e7158d
Soham Goswami <soham@chaka.info>
2022-10-15 16:06:29 +0530

    feat: refactored test code

--- internal/value/value_test.go
+++ internal/value/value_test.go
@@ -7,17 +7,16 @@ import (
 )
 
 var _ = Describe("Value", func() {
-	f32Value := value.NewFloat(34)
-	another := value.NewFloat(44)
+	first := value.NewFloat(34)
+	second := value.NewFloat(44)
 	Context("Of Float32", func() {
 		It("Should have a name method", func() {
-			Expect(f32Value.Name()).To(Equal("Float"))
+			Expect(first.Name()).To(Equal("Float"))
 		})
 		It("Should have a plus method", func() {
-			plus := f32Value.Plus(another)
+			plus := first.Plus(second)
 			result := value.NewFloat(78)
 			Expect(plus).To(Equal(result))
-			Expect(&plus).NotTo(Equal(f32Value))
 		})
 	})
 })
